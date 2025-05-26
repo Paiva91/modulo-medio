07f1c4a523c33ee3577a0a0b555db81ce2a38e4f
Paiva91 <darciohenripaiva@gmail.com>
2025-05-26 14:29:51 -0300

    Exercícios 26.05

---
+++ Exercício 02/par.js
@@ -0,0 +1,7 @@
+let num = parseInt(prompt("Digite um número:"));
+
+if (num % 2 === 0) {
+    console.log("O número " + num + " é par.");
+} else {
+    console.log("O número " + num + " é ímpar.");
+}
